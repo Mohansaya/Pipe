6787592422d7a180ca1d263026dae177645010fb
Mohansaya <91656863+Mohansaya@users.noreply.github.com>
2021-10-13 22:53:52 +0530

    Update mmm.groovy

--- mmm.groovy
+++ mmm.groovy
@@ -1,15 +1,20 @@
-pipline{
-    agent any 
-    stages{
-        stage('SCM'){
-            steps{
-                git url: "https://github.com/Mohansaya/Pipe.git"
+pipeline{
+    agent any
+        stages{
+            stage('SCM'){
+                steps{
+                    git url: ""
+                }
+            }
+            stage('ArchiveArtifacts'){
+                steps{
+                    archiveArtifacts '**/*.html'
+                }
+            }
+            stage('Build'){
+                steps{
+                    sshPublisher(publishers: [sshPublisherDesc(configName: 'webserver', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'apt-get update', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '**/*.html')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
+                }
             }
         }
-        stage(build){
-       steps{
-           sshPublisher(publishers: [sshPublisherDesc(configName: 'webserver', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'apt-get update', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '**/*.html')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
-       }
-        }
     }
-}
