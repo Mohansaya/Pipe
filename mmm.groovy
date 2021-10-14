5d017632eab1e3d3140c218936ab2eac204a8258
Mohansaya <91656863+Mohansaya@users.noreply.github.com>
2021-10-15 00:00:53 +0530

    Update mmm.groovy

--- mmm.groovy
+++ mmm.groovy
@@ -3,7 +3,7 @@ pipeline{
         stages{
             stage('SCM'){
                 steps{
-                    git url: "https://github.com/Mohansaya/Pipe.git"
+                    git url:"https://github.com/musthaq2810/webserver_pipeline.git"
                 }
             }
             stage('ArchiveArtifacts'){
@@ -13,7 +13,7 @@ pipeline{
             }
             stage('Build'){
                 steps{
-                    sshPublisher(publishers: [sshPublisherDesc(configName: 'webserver', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'apt-get update', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '**/*.html')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
+                    sshPublisher(publishers: [sshPublisherDesc(configName: 'webserver', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '**/*.html')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
                 }
             }
         }
